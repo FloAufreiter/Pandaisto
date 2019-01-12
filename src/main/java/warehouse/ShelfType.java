a8c4633077b171200b70f83711695616849c1bc0
FloAufreiter <florian.aufreiter@zive.at>
2019-01-12 15:37:31 +0100

    Add ShelfType

---
+++ src/main/java/warehouse/ShelfType.java
@@ -0,0 +1,23 @@
+package warehouse;
+
+import AGV.Location;
+
+public class ShelfType {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public Location.LocationType getType() {
+        return type;
+    }
+
+    private Location.LocationType type;
+
+    ShelfType(int id , Location.LocationType type) {
+        id = id;
+        type = type;
+
+    }
+}
